81924f206aa6592a65f261220b73751f22e8939c
Tal Cohen <cohental63@gmail.com>
2024-07-17 12:52:41 +0300

    add types for DB entities and API

--- src/types.ts
+++ src/types.ts
@@ -6,28 +6,63 @@ export interface Token {
   subject?: string;
 }
 
-export interface UserAPI {
+/* Database result interfaces */
+
+export interface UserDB {
   id: string;
+  name: string;
   email: string;
   image: string;
 }
 
+export interface RestaurantDB {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+  date_added: Date;
+  edited_at: Date | null;
+  added_by: UserDB;
+  address: string;
+  reviews: ReviewsDB[];
+  categories: string[];
+  images?: string[];
+}
+
+export interface ReviewsDB {
+  id: string;
+  text: string;
+  rating: number;
+  author: UserDB;
+  date_added: Date;
+  edited_at: Date | null;
+  restaurant_id: string;
+  images?: string[];
+}
+
+/* API response interfaces */
+
+export interface UserAPI extends UserDB {}
+
 export interface RestaurantAPI {
-  id?: string;
+  id: string;
   name: string;
   address: string;
   lat: number;
   lng: number;
-  addedBy: string;
+  addedBy: UserDB;
   images: string[];
-  reviews?: ReviewAPI[];
-  dateAdded?: Date;
-  dateEdited?: Date;
+  reviews: ReviewAPI[];
+  categories: string[];
+  dateAdded: Date;
+  dateEdited: Date | null;
 }
 
 export interface ReviewAPI {
   id: string;
   text: string;
   rating: number;
-  addedBy: UserAPI;
+  author: UserAPI;
+  dateAdded: Date;
+  dateEdited: Date | null;
 }
